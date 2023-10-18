d4006d0034c4972bc962940814c86a279d18d7fb
Tonmoy Biswas <tonmoybiswas19122002@gamil.com>
2023-10-18 19:04:22 +0530

    modified loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,33 +1,34 @@
 import './Loader.css'
-const Loader=()=>{
-    return (
-        <>
-        <div className="loader-container">
-         <div className="wrapper">
-            <div className="loader">
-               <div className="dot first"></div>
+const Loader = () => {
+   return (
+      <>
+         <div class="background"></div>
+         <div className="loader-container">
+            <div className="wrapper">
+               <div className="loader">
+                  <div className="dot first"></div>
+               </div>
+               <div className="loader">
+                  <div className="dot second"></div>
+               </div>
+               <div className="loader">
+                  <div className="dot third"></div>
+               </div>
+               <div className="loader">
+                  <div className="dot fourth"></div>
+               </div>
+               <div className="loader">
+                  <div className="dot fifth"></div>
+               </div>
+               <div className="loader">
+                  <div className="dot sixth"></div>
+               </div>
             </div>
-            <div className="loader">
-               <div className="dot second"></div>
-            </div>
-            <div className="loader">
-               <div className="dot third"></div>
-            </div>
-            <div className="loader">
-               <div className="dot fourth"></div>
-            </div>
-            <div className="loader">
-               <div className="dot fifth"></div>
-            </div>
-            <div className="loader">
-               <div className="dot sixth"></div>
+            <div className="text">
+               Please wait
             </div>
          </div>
-         <div className="text">
-            Please wait
-         </div>
-      </div>
-        </>
-    )
+      </>
+   )
 }
 export default Loader
